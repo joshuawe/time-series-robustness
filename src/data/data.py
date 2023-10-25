9910b3eb434df4da16f1d72ce90cc2ef0b520b03
joshuawe <joshua.wendland@tum.de>
2023-10-25 18:38:49 +0200

    correct correlation and

--- src/data/data.py
+++ src/data/data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 from typing import Tuple
 
@@ -52,7 +53,7 @@ def create_corr_plot(df: pd.DataFrame, save_path:str=None) -> None:
     save_path : str, optional
         The path to where plot can be stored, by default None
     """
-    corr = df.corr()
+    corr = df.corr(method='spearman')
     fig = plt.figure(figsize=(10,9))
     ax = sns.heatmap(corr, cmap="RdBu_r", vmin=-1, vmax=1)
     txt = ax.set_title("Correlation Matrix", fontsize=16)
@@ -60,3 +61,29 @@ def create_corr_plot(df: pd.DataFrame, save_path:str=None) -> None:
     if save_path is not None:
         plt.savefig(save_path, dpi=200, bbox_inches='tight')
     return
+
+def pca_on_data(df: pd.DataFrame, n_components: int) -> pd.DataFrame:
+    """
+    Perform PCA on the given dataframe.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The dataframe to perform PCA on.
+    n_components : int
+        The number of components to keep OR float value representing percentage of information to keep.
+
+    Returns
+    -------
+    pd.DataFrame
+        The dataframe with PCA performed.
+    """
+    from sklearn.decomposition import PCA
+    pca = PCA(n_components=n_components)
+    # pca.fit(df.values)
+    # X_new = pca.transform(df.values)
+    X_new = pca.fit_transform(df.values)
+    X_new = pd.DataFrame(X_new)
+    
+    print(f"Performed PCA. \nKept an Explained variance ratio of : {np.sum(pca.explained_variance_ratio_):.2%} \nNumber of components: {pca.n_components_} / {df.shape[1]}")
+    return X_new
